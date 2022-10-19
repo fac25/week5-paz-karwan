909fef79f7e68723969f17a50771c4ab33a1ee13
mariapaz <mariapazgar@gmail.com>
2022-10-19 10:30:42 +0100

    added homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
+const Home = ({ handleNameChange }) => {
+  // function handleNameChange(e) {
+  //   e.preventDefault();
+  //   setUsername(e.target.value);
+  // }
+  return (
+    <div>
+      <h1>Hangman</h1>
+      <label htmlFor="name">Give us your name</label>
+      <input type="text" name="name" id="name" onBlur={handleNameChange} />
+      <Link to="/game">PLAY</Link>
+    </div>
+  );
+};
 
-const Home = () => {
-    return (
-      <div>Home!</div>
-    )
-  }
-  
-  
-  export default Home
+export default Home;
