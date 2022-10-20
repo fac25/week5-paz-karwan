487cb58dee29a9bd671f5d40cff16903d1a3aba5
karwanm <karwan.ismo@gmail.com>
2022-10-20 16:46:32 +0100

    clean

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ const Home = ({ handlePlay }) => {
   return (
     <main>
       <label htmlFor="name">Give us your name</label>
-      <input type="text" name="name" id="name" />
+      <input type="text" name="name" id="name"   />
       <button onClick={handlePlay}><Link to="/game">PLAY</Link></button>
     </main>
   );
