9da303172c9ddf722e018cc0eb672a193f422734
Paz <mariapazgar@gmail.com>
2022-10-20 16:24:03 +0100

    styles v1.0

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -61,74 +61,80 @@ const Game = ({ word, randomWord }) => {
 
   return (
     <main>
-      <p>{word}</p>
-      <section>
-        {isWinner ? (
-          <h3>You won!</h3>
-        ) : isLoser ? (
-          <h3>You lost!</h3>
-        ) : (
-          <p>You have {chance} tries left </p>
-        )}
-      </section>
+      <div className="game-container">
+        <section className="control-panel">
+          <p>{word}</p>
+          {isWinner ? (
+            <h3>You won!</h3>
+          ) : isLoser ? (
+            <h3>You lost!</h3>
+          ) : (
+            <p>
+              You have <span className="bold">{chance}</span> tries left{" "}
+            </p>
+          )}
+        </section>
 
-      <div className="">
-        <section className="letters">
-          {letters.map((letter, index) => {
-            return (
-              <span className="test" key={index}>
-                {letter}
-              </span>
-            );
-          })}
-        </section>
-        <section
-          className="keyboard-layout"
-          id="keyboard"
-          onClick={handleChange}
-        >
-          <div className="row-1">
-            <button>Q</button>
-            <button>W</button>
-            <button>E</button>
-            <button>R</button>
-            <button>T</button>
-            <button>Y</button>
-            <button>U</button>
-            <button>I</button>
-            <button>O</button>
-            <button>P</button>
-          </div>
-          <div className="row-2">
-            <button>A</button>
-            <button>S</button>
-            <button>D</button>
-            <button>F</button>
-            <button>G</button>
-            <button>H</button>
-            <button>J</button>
-            <button>K</button>
-            <button>L</button>
-          </div>
-          <div className="row-3">
-            <button>Z</button>
-            <button>X</button>
-            <button>C</button>
-            <button>V</button>
-            <button>B</button>
-            <button>N</button>
-            <button>M</button>
-          </div>
-        </section>
-      </div>
-      {playAgain ? (
-        <div className="modal">
-          <h3>Want to play more?</h3>
-          <button onClick={handlePlayAgain}>Play Again</button>
+        <div className="">
+          <section className="letters">
+            {letters.map((letter, index) => {
+              return (
+                <span className="letter-placeholder" key={index}>
+                  {letter}
+                </span>
+              );
+            })}
+          </section>
+          <section
+            className="keyboard-layout"
+            id="keyboard"
+            onClick={handleChange}
+          >
+            <div className="row-1">
+              <button>Q</button>
+              <button>W</button>
+              <button>E</button>
+              <button>R</button>
+              <button>T</button>
+              <button>Y</button>
+              <button>U</button>
+              <button>I</button>
+              <button>O</button>
+              <button>P</button>
+            </div>
+            <div className="row-2">
+              <button>A</button>
+              <button>S</button>
+              <button>D</button>
+              <button>F</button>
+              <button>G</button>
+              <button>H</button>
+              <button>J</button>
+              <button>K</button>
+              <button>L</button>
+            </div>
+            <div className="row-3">
+              <button>Z</button>
+              <button>X</button>
+              <button>C</button>
+              <button>V</button>
+              <button>B</button>
+              <button>N</button>
+              <button>M</button>
+            </div>
+          </section>
         </div>
-      ) : (
-        ""
-      )}
+        {playAgain ? (
+          <div className="modal center">
+            <div className="modal-content">
+              <h3>Want to play more?</h3>
+              <button onClick={handlePlayAgain}>Play Again</button>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </main>
   );
 };
